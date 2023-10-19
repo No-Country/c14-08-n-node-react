58d0dee28dfcbce1b1a5f186ba0923da643c913e
sanieni6 <luissanchezzamora23@gmail.com>
2023-10-18 22:21:00 -0500

    Install dependencie and adjust the store

--- client/store/user.ts
+++ client/store/user.ts
@@ -1,22 +1,44 @@
+import { StateCreator } from 'zustand'
 import axios from "axios";
 import { endpoints } from "../constants/endpoints";
-export const CreateUser = (set) => ({
+
+export interface UserState {
+  currentUser: any;
+  role: any;
+  token: any;
+  signUpClient: (data: object) => Promise<void>;
+  login: (data: object) => Promise<void>;
+}
+
+export const CreateUser: StateCreator<UserState> = (set: any) => ({
   currentUser: null,
   role: null,
   token: null,
   signUpClient: async (data) => {
+    console.log(data);
     try {
+      console.log(data);
       const res = await axios.post(endpoints[0].url, data);
       set({
         currentUser: res.data.user,
         role: res.data.role,
         token: res.data.token,
       });
-      localStorage.setItem("token", res.data.token);
-      localStorage.setItem("role", res.data.role);
-      localStorage.setItem("user", JSON.stringify(res.data.user));
     } catch (err) {
       console.log(err);
     }
   },
+  login: async (data) => {
+    console.log(data);
+    try {
+      const res = await axios.post(endpoints[1].url, data);
+      set({
+        currentUser: res.data.user,
+        role: res.data.role,
+        token: res.data.token,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 });
