625fc27cf51ae507a9bf02e034240be70e7f2a7c
jp-quintana <juanquintana1996@gmail.com>
2023-11-03 09:58:54 -0300

    Fix For Deploy

--- client/app/abogado/turnos/page.tsx
+++ client/app/abogado/turnos/page.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-
+import { BookingList } from "@/components";
 const page = () => {
-  return <div>page</div>;
+  return (
+    <section className="relative min-h-screen bg-gray-400">
+      <BookingList />
+    </section>
+  );
 };
 
 export default page;
