19fa2952edb153624de1f6bc1d78347179f97d49
jp-quintana <juanquintana1996@gmail.com>
2023-11-03 22:34:49 -0300

    Add Fix

--- client/components/LawyerList.tsx
+++ client/components/LawyerList.tsx
@@ -45,17 +45,19 @@ const LawyerList = ({
   }, [unformattedSelectedCategory, selectedFormat]);
 
   return (
-    <div className="main-container relative my-[46px] grid min-h-screen grid-cols-2 gap-[10px] max-md:flex max-md:flex-col">
-      {isLoading && (
-        <Spinner className="absolute left-0 right-0 top-0 m-auto" />
-      )}
-      {!isLoading && (
-        <>
-          {lawyers.map((lawyer) => (
-            <LawyerCard key={lawyer.id} {...lawyer} />
-          ))}
-        </>
-      )}
+    <div className="min-h-screen ">
+      <div className="main-container relative my-[46px] grid grid-cols-2 gap-[10px] max-md:flex max-md:flex-col">
+        {isLoading && (
+          <Spinner className="absolute left-0 right-0 top-0 m-auto" />
+        )}
+        {!isLoading && (
+          <>
+            {lawyers.map((lawyer) => (
+              <LawyerCard key={lawyer.id} {...lawyer} />
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
 };
